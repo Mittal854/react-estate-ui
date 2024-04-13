5181fae6d5949a78e11ea73b2639c8dd0dda3c0c
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-04-13 21:13:50 +0530

    43.12

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,15 @@ import './layout.css';
 function App() {
   return (
     <div className="layout">
+      <div className="navbar">
       <Navbar/>
+      </div>
+      
+      <div className="content">
+      <homePage/>
+      </div>
     </div>
-  )
+  );
 }
 
 export default App
---
+++ src/homePage.jsx
@@ -0,0 +1,41 @@
+import './homePage.css'
+
+function HomePage(){
+    return(
+        <div className='homePage'>
+            <div className="textcontainer">
+                <div className="wrapper">
+                    <h1 className="title">
+                        Find Real Estate &  Get Your Dream PLace
+                    </h1>
+                    <p>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                        Tempore architecto qui asperiores ut numquam iusto at 
+                        molestias odio sunt corrupti accusamus saepe nam eos excepturi 
+                        deserunt quaerat, magnam doloribus provident!
+                    </p>
+                    <SearchBar/>
+                    <div className="boxes">
+                        <div className="box">
+                            <h1>16+</h1>
+                            <h2>Years Of Experience</h2>
+                        </div>
+                        <div className="box">
+                            <h1>200</h1>
+                            <h2>Award Gained</h2>
+                        </div>
+                        <div className="box">
+                            <h1>2000</h1>
+                            <h2>Property Ready</h2>
+                        </div>
+                    </div>
+                </div>  
+            </div>
+            <div className="imgcontainer">
+                <img src="/bg.png" alt="" />
+            </div>
+        </div>
+    )
+}
+
+export default HomePage
