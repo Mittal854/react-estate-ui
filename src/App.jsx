92a6299ceb36a9a00fd4bc1d1dbd9602fb1381ea
DaanushiSharma <daanushi260104@gmail.com>
2024-04-16 19:46:28 +0530

    1:10:30

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-// import Navbar from "./components/navbar/navbar";
+
 import HomePage from "./routes/homepage/homePage";
 // import './layout.css';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
