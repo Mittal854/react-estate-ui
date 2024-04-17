ef58a35f22061c5440f10e62ed52c20e63a9edb5
Mittal854 <bhuveshmittal854@gmail.com>
2024-04-17 22:55:16 +0530

    scrollbar fully functional now issue resolved

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import "./index.css"
-import HomePage from './routes/homepage/homePage.jsx'
+// import HomePage from './routes/homepage/homePage.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- src/routes/listpage/listPage.jsx
+++ src/routes/listpage/listPage.jsx
@@ -1,24 +1,26 @@
 import { listData } from "../../Library/dummyData";
-import "./listPage.css";    
-import Filter from '../../components/filter/filter'
-import Card from '../../components/card/card'
+import "./listPage.css";
+import Filter from "../../components/filter/filter";
+import Card from "../../components/card/card";
 
-function ListPage(){
-    const data = listData;
+function ListPage() {
+  const data = listData;
 
-    return(
-        <div className="listPage">
-            <div className="listContainer">
-                <div className="wrapper">
-                    <Filter/>
-                    {data.map(item =>(
-                        <Card key={item.id} item={item}/>
-                    ))}
-                </div>
-            </div>
-            <div className="mapContainer">Map</div>
+  return (
+    <div className="listPage">
+      <div className="listContainer">
+        <div className="wrapper">
+          <Filter />
+          {data.map((item) => (
+            <Card key={item.id} item={item} />
+          ))}
         </div>
-    )
+      </div>
+      <div className="mapContainer">
+       MAP
+      </div>
+    </div>
+  );
 }
 
 export default ListPage;
