09a7a03bc6c89520babc57d7bacde0fb423d4f1c
DaanushiSharma <daanushi260104@gmail.com>
2024-04-18 15:24:37 +0530

    2:29:00

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import {
 import ListPage from "./routes/listpage/listPage";
 import Layout from "./routes/layout/layout";
 import SinglePage from "./routes/SinglePage/singlePage";
+// import Slider from "./routes/slider/Slider";
 
 function App() {
   const router1 = createBrowserRouter([
@@ -30,6 +31,7 @@ function App() {
           path: "/:id",
           element: <SinglePage />,
         }
+        
       ],
     },
   ]);
--- src/components/pin/pin.jsx
+++ src/components/pin/pin.jsx
@@ -2,26 +2,6 @@ import { Marker, Popup } from "react-leaflet";
 import "./pin.css";
 import { Link } from "react-router-dom";
 
-import L from "leaflet";
-import markerIcon from "leaflet/dist/images/marker-icon.png";
-import markerIconRetina from "leaflet/dist/images/marker-icon-2x.png";
-import markerShadow from "leaflet/dist/images/marker-shadow.png";
-
-// Create a custom marker icon
-const customMarkerIcon = L.icon({
-  iconUrl: markerIcon,
-  iconRetinaUrl: markerIconRetina,
-  shadowUrl: markerShadow,
-  iconSize: [25, 41], // size of the icon
-  iconAnchor: [12, 41], // point of the icon which will correspond to marker's location
-  popupAnchor: [1, -34], // point from which the popup should open relative to the iconAnchor
-  tooltipAnchor: [16, -28], // point from which the tooltip should open relative to the iconAnchor
-  shadowSize: [41, 41], // size of the shadow
-});
-
-// Set the default marker icon to the custom marker icon
-L.Marker.prototype.options.icon = customMarkerIcon;
-
 function Pin({ item }) {
   return (
     <Marker position={[item.latitude, item.longitude]}>
