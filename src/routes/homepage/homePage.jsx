3ee8148cec9bb0a6094d59f7d960e233ded995ff
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-04-26 13:47:06 +0530

    rental listing

--- src/routes/homepage/homePage.jsx
+++ src/routes/homepage/homePage.jsx
@@ -3,6 +3,7 @@ import SearchBar from '../../components/searchbar/SearchBar'
 
 function HomePage(){
     return(
+        <>
         <div className='homePage'>
             <div className="textcontainer">
                 <div className="wrapper">
@@ -32,7 +33,24 @@ function HomePage(){
             <div className="imgcontainer">
                 <img src="/bg.png" alt="" />
             </div>
+            
         </div>
+        
+        <div className='heading1'>
+            <h2 class="h1">The Most Rental Listings</h2>
+            <p className='p1'>Choose from over 1 million apartments, houses, condos, and townhomes for rent.</p>
+        </div>
+
+        <div class="container">
+            <div class="listing">
+                <img src="https://media.istockphoto.com/id/510231606/photo/backpackers-looking-for-apartment-online.jpg?s=612x612&w=0&k=20&c=bzmOBqxu54XX3hmW-TfQQq7CBRg_IG3o3n3XT_No-go=" alt="" />
+                <div class="details">
+                    <h2>Renting made simple</h2>
+                    <p>Browse the highest quality listings, apply online, sign your lease, and even pay your rent from any device.</p>
+                </div>
+            </div>
+        </div>
+</>
     )
 }
 
