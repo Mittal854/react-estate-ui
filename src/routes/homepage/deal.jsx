c732e5ab7e770eeaaecc9abc3cba9e13f7123044
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-04-28 14:23:25 +0530

    cm

---
+++ src/routes/homepage/deal.jsx
@@ -0,0 +1,22 @@
+import './deal.css'
+
+function Deal() {
+    return (
+        <div class="deal">
+     <div class="content">
+          <h3>Deal Of The Day</h3>
+          <h1>Upto 50% off</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, atque eaque repellendus quos aperiam magnam 
+               inventore, beatae fugiat unde iusto nihil exercitationem assumenda labore incidunt vel excepturi perferendis 
+               cum voluptatum.</p>
+               {/* <a href="#" class="btn">Shop Now</a> */}
+     </div>
+     <div class="image">
+          <img src="https://3.imimg.com/data3/IJ/FJ/MY-9086795/property-solution-500x500.jpg" alt=""/>
+     </div>
+</div>
+
+    );
+};
+
+export default Deal;
